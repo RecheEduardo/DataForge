02ad829ea1069c4e32503118fb6ef4998cd90cad
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-29 19:34:04 -0300

    feat: substituindo abreviações da tabela conforme dados do rodapé do PDF

--- data-transformation/transformData.py
+++ data-transformation/transformData.py
@@ -18,4 +18,33 @@ with pdfplumber.open(pdf_file_path) as pdf:
                 # substitui quebras de linha ("\n") por espaços em celulas que são strings.
                 row = [cell.replace("\n", " ") if isinstance(cell, str) else cell for cell in row]
                 # adiciona a linha para a lista de dados
-                data.append(row) 
+                data.append(row)
+
+
+# (2) segunda parte - substituiçao das abreviaçoes da tabela
+
+# primeira linha como cabeçalho
+header = data[0]  
+
+# remove a primeira linha dos dados para evitar duplicação do cabeçalho
+data = data[1:]  
+
+abreviacoes = {
+    "OD": "Seg. Odontológica",
+    "AMB": "Seg. Ambulatorial",
+    "HCO": "Seg. Hospitalar Com Obstetrícia",
+    "HSO": "Seg. Hospitalar Sem Obstetrícia",
+    "REF": "Plano Referência",
+    "PAC": "Procedimento de Alta Complexidade",
+    "DUT": "Diretriz de Utilização"
+}
+
+# substitui as abreviações no header
+header = [abreviacoes.get(col, col) for col in header]
+
+for row in data:
+    for i, cell in enumerate(row):
+        # para cada celula das linhas da tabela
+        # se o dado for igual a abreviação, sera substituido
+        if cell in abreviacoes:
+            row[i] = abreviacoes[cell]

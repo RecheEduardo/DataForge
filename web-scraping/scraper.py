7bc29e23970483a2c8a85287756997d1fb18518e
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-29 15:16:13 -0300

    feature: script de download dos anexos da página para diretório dentro do web-scrapper

--- web-scraping/scraper.py
+++ web-scraping/scraper.py
@@ -1,8 +1,13 @@
 import requests
-# lib de acesso aos dados do html do GOV
-from bs4 import BeautifulSoup 
+import os
 
-## primeira parte - acesso para a pagina do GOV
+# lib de acesso aos dados do html do GOV
+from bs4 import BeautifulSoup
+
+# lib para operar arquivos, será utilizada para compactação
+import shutil
+
+## (1) primeira parte - acesso para a pagina do GOV
 
 URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
@@ -16,7 +21,7 @@ else:
     exit() # encerra o script se nao tiver acesso
 
 
-## segunda parte - buscar todos os links da pagina
+## (2) segunda parte - buscar todos os links da pagina
 
 pdf_links = []
 
@@ -29,3 +34,29 @@ for link in soup.find_all("a", href=True):
 
 # mostra na tela os links encontrados
 print("links dos arquivos PDF encontrados: ", pdf_links)
+
+
+## (3) terceira parte - baixar os pdfs encontrados
+
+# cria a pasta 'downloads' caso nao exista
+os.makedirs("downloads", exist_ok=True)
+
+# loop que percorre o array de links dos arquivos pdf
+for pdf_url in pdf_links:
+    if pdf_url.startswith("/"):                     # se o link for relativo ao dominio do GOV 
+        pdf_url = "https://www.gov.br" + pdf_url    # o if transforma ele em um link completo
+    
+    # cria o arquivo dentro de downloads
+    file_name = os.path.join("downloads", pdf_url.split("/")[-1])
+    # a url é parseada e pega o nome após o ultimo '/' ou seja, o nome do arquivo PDF no site
+
+    pdf_response = requests.get(pdf_url) # realiza o método GET para baixar cada PDF da pagina
+
+    if pdf_response.status_code == 200: # 200 - download aconteceu
+        with open(file_name, "wb") as pdf_file:
+            # abre o arquivo criado e grava o conteudo do PDF baixado
+            pdf_file.write(pdf_response.content)
+
+        print(f"Arquivo baixado! -> {file_name}")
+    else:
+        print(f"ERRO! Não foi possível baixar {pdf_url}")

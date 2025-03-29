82b15f32db110ea380e52dc3cf31e61002e259bc
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-29 13:11:00 -0300

    feat: funcionalidade de busca e registro dos pdfs da pagina

--- web-scraping/scraper.py
+++ web-scraping/scraper.py
@@ -13,3 +13,19 @@ if response.status_code == 200: # 200 - solicitacao aconteceu
     print("Página acessada!")
 else:
     print(f"ERRO! Não foi possivel acessar a página: {response.status_code}")
+    exit() # encerra o script se nao tiver acesso
+
+
+## segunda parte - buscar todos os links da pagina
+
+pdf_links = []
+
+# loop que busca todas as tags de link do html 
+# e se tiver href de um pdf, adiciona o link no array
+for link in soup.find_all("a", href=True):
+    href = link["href"]
+    if href.endswith(".pdf"):
+        pdf_links.append(href)
+
+# mostra na tela os links encontrados
+print("links dos arquivos PDF encontrados: ", pdf_links)

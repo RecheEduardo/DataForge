4ed4da7743201db41bcf491e56354d5ae7eaea57
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-30 17:05:45 -0300

    feature: implementando conexão axios com o servidor flask para busca de registros

--- api/frontend/src/components/Search.vue
+++ api/frontend/src/components/Search.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const query = ref<string>('Filantropia')
+
+const search = async (): Promise<void> => {
+  try {
+    const response = await axios.get(`http://127.0.0.1:5000/search?query=${query.value}`)
+    console.log('Resposta da API:', response.data)
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error)
+  }
+}
+</script>
+
 <template>
   <div>
-    Componente de Busca
+    <button @click="search" class="btn btn-lg btn-primary">Buscar</button>
   </div>
 </template>

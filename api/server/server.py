93ef5242a8adfea21f2eff13f8d154402ec19e7b
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-30 21:38:40 -0300

    feature: criando novas rotas da API para busca no back end por outros parametros

--- api/server/server.py
+++ api/server/server.py
@@ -17,8 +17,8 @@ CORS(app)
 data = pd.read_csv('../../database/downloads/operadoras/Relatorio_cadop.csv', encoding='utf-8', delimiter=';')
 
 # cria uma rota chamada /search que responde a requisiçoes GET
-@app.route('/search', methods=['GET'])
-def search():
+@app.route('/searchModalidade', methods=['GET'])
+def search_modalidade():
     # tratamento de dados - pega o que foi passado na url como query e remove espaços
     query = request.args.get('query', '').strip()
     
@@ -44,6 +44,52 @@ def search():
         return jsonify(cleaned_records)
     
     # se nao tiver query nenhuma, retorna lista vazia
+    return jsonify([])
+
+# mesma lógica para outras categorias de pesquisa: UF e Região
+
+@app.route('/searchUF', methods=['GET'])
+def search_UF():
+    # tratamento de dados - pega o que foi passado na url como query e remove espaços
+    query = request.args.get('query', '').strip()
+    
+    # verifica se foi passado alguma coisa na query
+    if query:
+        # filtra os dados buscando na coluna 'UF' todas as linhas que contem a query
+        filtered = data[data['UF'].str.contains(query, case=False, na=False)]
+
+        records = filtered.to_dict(orient='records')
+
+        cleaned_records = [ # limpeza de NaN para ''
+            { key: ('' if pd.isna(value) else value) for key, value in record.items() }
+            for record in records
+        ]
+        
+        # pega a lista de dicionario e retorna convertendo pro formato json
+        return jsonify(cleaned_records)
+    
+    return jsonify([])
+
+@app.route('/searchRegiao', methods=['GET'])
+def search_Regiao():
+    # tratamento de dados - pega o que foi passado na url como query e remove espaços
+    query = request.args.get('query', '').strip()
+    
+    # verifica se foi passado alguma coisa na query
+    if query:
+        # filtra os dados buscando na coluna 'Regiao' todas as linhas que contem a query
+        filtered = data[data['Regiao_de_Comercializacao'].str.contains(query, case=False, na=False)]
+
+        records = filtered.to_dict(orient='records')
+
+        cleaned_records = [ # limpeza de NaN para ''
+            { key: ('' if pd.isna(value) else value) for key, value in record.items() }
+            for record in records
+        ]
+        
+        # pega a lista de dicionario e retorna convertendo pro formato json
+        return jsonify(cleaned_records)
+    
     return jsonify([])
 
 # modo debug pra mostrar erros no terminal

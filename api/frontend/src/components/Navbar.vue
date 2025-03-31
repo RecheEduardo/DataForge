21d138a39bf3e1a17193b7f9e3b975a0c4202250
recheeduardo <eduardorechemartins2006@gmail.com>
2025-03-31 18:18:27 -0300

    feature: adicionando links de contato na navbar

--- api/frontend/src/components/Navbar.vue
+++ api/frontend/src/components/Navbar.vue
@@ -1,8 +1,16 @@
 <template>
-    <nav class="bg-dark py-3 px-5 fadeInDown">
+    <nav class="bg-dark py-3 px-5 fadeInDown d-flex justify-content-between align-items-center">
         <h1 class="text-light fw-bold fadeInDown">
             <i class="fa-solid fa-gears"></i>
             DataForge
         </h1>
+        <div class="d-flex gap-4 align-items-center mb-0 list-unstyled fs-1">
+            <a href="https://www.linkedin.com/in/eduardorechemartins/" class="text-light fadeInDown">
+                <i class="fa-brands fa-square-github"></i>
+            </a>
+            <a href="https://github.com/RecheEduardo" class="text-light fadeInDown">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
     </nav>
 </template>

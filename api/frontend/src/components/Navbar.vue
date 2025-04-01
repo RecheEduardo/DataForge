abe8c773bb3728962de1d357728825f979472a62
recheeduardo <eduardorechemartins2006@gmail.com>
2025-04-01 20:21:02 -0300

    feat: alteração visual da navbar

--- api/frontend/src/components/Navbar.vue
+++ api/frontend/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="bg-dark py-3 px-5 fadeInDown d-flex justify-content-between align-items-center">
         <h1 class="text-light fw-bold fadeInDown">
             <i class="fa-solid fa-gears"></i>
-            DataForge
+            DataForge | Eduardo Reche
         </h1>
         <div class="d-flex gap-4 align-items-center mb-0 list-unstyled fs-1">
             <a href="https://github.com/RecheEduardo" target="_blank" class="text-light fadeInDown upScale">
